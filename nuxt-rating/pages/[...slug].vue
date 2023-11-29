b7e775dab9ab4301915b7b4a031bc49b6b2eaccd
aaaaa-pi <770358634@qq.com>
2023-11-29 14:40:17 +0800

    wip: 🚧存储投票结果功能

--- nuxt-rating/pages/[...slug].vue
+++ nuxt-rating/pages/[...slug].vue
@@ -10,7 +10,7 @@
     <div class="text-center">
       Your vote: {{ myRate || '🤔️' }}
     </div>
-    <div class="rating flex-row-reverse justify-center items-center">
+    <div class="rating flex-row-reverse justify-center items-center" @change="onChange">
       <input class="mask mask-star-2 w-10 h-10"
         v-for="item in 5"
         type="radio"
@@ -25,9 +25,27 @@
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
+const path = route.path;
 const emojis = ['🤔️', '💩', '👎', '😄', '😍', '🎉'];
 const emojiWrapper = ref<HTMLDivElement>();
 const myRate = ref<number>(0);
+const {data:rateDate,pending} = await useFetch('/api/rate',{
+  query:{
+    uid: path,
+  },
+  default() {
+    return { r1: 0, r2: 0, r3: 0, r4: 0, r5: 0 };
+  }
+})
+const onChange = ():void => {
+  if(!emojiWrapper.value) return 
+
+  emojiWrapper.value.scrollTo({
+    top: myRate.value * emojiWrapper.value.clientHeight,
+    behavior: "smooth"
+  })
+}
 </script>
 
 <style scoped></style>
---
+++ nuxt-rating/server/api/rate.post.ts
@@ -0,0 +1,24 @@
+import {H3Event} from 'h3';
+import Redis from 'ioredis';
+import { getRedisKey } from '~/utils';
+export default defineEventHandler(async(event: H3Event): Promise<unknown> => {
+    const redis = new Redis(process.env.REDIS_URL as string);
+    const {uid} = getQuery(event);
+    const redisKey = getRedisKey(uid as string);
+    let data;
+    try{
+        data = await redis.get(redisKey)
+    }catch(e){
+        throw createError({
+            statusCode: 400,
+            statusMessage: 'Failed to fetch stored data.' + 
+            ((e as Error).message || Object.prototype.toString.call(e)),
+        });
+    };
+    redis.quit();
+    setHeaders(event,{
+        'content-type': 'application/json',
+        'cahce-control': 'public,s-maxage=1800,stale-while-revalidate=2400',
+    });
+    return data;
+});
